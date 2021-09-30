a4cc281ed70fd3946890117fccda33bff6150d25
Daniel Molina <dmolcap@gmail.com>
2021-09-30 22:20:34 +0200

    Add createNewBlock method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -12,3 +12,19 @@ function Blockchain() {
 
 //    // We'd place all the methods in here
 // }
+
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+   const newBlock = { // This is gonna be a block inside our blockchain
+      index: this.chain.length + 1, // Block number
+      timestamp: Date.now(),
+      transactions: this.newTransaction, // All the tnx waiting to be included in a new block
+      nonce: nonce, // It's proof (PoW) (a number) that we created this block in a legitimate way
+      hash: hash, // We get this passing our newTransactions into a hashing function
+      previousBlockHash: previousBlockHash
+   }
+
+   this.newTransaction = []; // we empy out the pending transactions after we include them in the new block
+   this.chain.push(newBlock);
+
+   return newBlock;
+}
