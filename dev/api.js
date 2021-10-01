2e849a307a3fd60b42e14584b872cd94de6dbe9e
Daniel Molina <dmolcap@gmail.com>
2021-10-01 16:50:05 +0200

    Add GET /blockchain endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+app.use(express.json());
+app.use(express.urlencoded({   extended: true }));
  
 // Send back the entire blockchain
 app.get('/blockchain', function (req, res) {
-  
+  res.send(bitcoin);
 });
 
 // End-point to create a transaction in the blockchain
 app.post('/transaction', function (req, res) {
-   
+   console.log(req.body);
+   res.send(`The amount of the transaction is ${req.body.amount} bitcoins.`);
 });
 
 // End-point to mine/create a new block
